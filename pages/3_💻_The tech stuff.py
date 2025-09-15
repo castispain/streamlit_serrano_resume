f3db9582f622fd9fc02f04184b75519d231481db
castispain <casti_spain@hotmail.com>
2025-09-15 10:12:32 -0700

    change images path to forward slash

--- pages/3_💻_The tech stuff.py
+++ pages/3_💻_The tech stuff.py
@@ -17,33 +17,33 @@ My current intersts lay in leveraging big data with Spark for predicting modelli
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Geospatial platform")
-    st.image("images\\gis_platform.jpg", use_container_width=True)
+    st.image("images/gis_platform.jpg", use_container_width=True)
     st.markdown("Desktop and Web for GIS")
 with col2:
     st.subheader("Customizing GIS")
-    st.image("images\\etl_workflow.jpg", use_container_width=True)
+    st.image("images/etl_workflow.jpg", use_container_width=True)
     st.markdown("Custom web widgets with VertiGIS workflows")
 st.markdown("---")
 
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Raster and 3D analysis")
-    st.image("images\\iec_3d.PNG", use_container_width=True)
+    st.image("images/iec_3d.PNG", use_container_width=True)
     st.markdown("Complex Terrain Analysis for IEC Norm")
 
 with col2:
     st.subheader("Vector analysis and statistics")
-    st.image("images\\lcoe_bl_2.png", use_container_width=True)
+    st.image("images/lcoe_bl_2.png", use_container_width=True)
     st.markdown("Least Cost of Energy (LCOE)")
 st.markdown("---")
 
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Big Geospatial Data Processing")
-    st.image("images\\big data.png", use_container_width=True)
+    st.image("images/big data.png", use_container_width=True)
     st.markdown("Grid schema for parallel processing")
 
 with col2:
     st.subheader("Systems Integration")
-    st.image("images\\lcm 1.jpg", use_container_width=True)
+    st.image("images/lcm 1.jpg", use_container_width=True)
     st.markdown("ArcGIS Enterprise and Malbek integration")
